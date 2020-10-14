4e43bdc64fec001a4cd4e869285cdf2af1035d8b
bosshentai <baptistamhernani@gmail.com>
2020-10-14 19:46:19 -0100

    boa

--- public/script/page-orphanages.js
+++ public/script/page-orphanages.js
@@ -27,7 +27,7 @@ const popup = L.popup({
     className: 'map-popup',
     minWidth: 240,
     minHeight: 240
-}).setContent('Lar das meninas <a href="orphanage.html?id=1"class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>');
+}).setContent('Lar das meninas <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>');
 
 
 L.marker([-27.2151682, -49.642853],{ icon})
