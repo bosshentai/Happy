9f0bece44cdab61b0385832cd6e9c10fab7f0576
bosshentai <baptistamhernani@gmail.com>
2020-10-15 10:35:44 -0100

    aula 2 completo

--- public/script/page-orphanage.js
+++ public/script/page-orphanage.js
@@ -12,7 +12,7 @@ const options = {
 }
 
 //create map
-const mymap = L.map("mapid", options).setView([-27.2151682,-49.642853], 15);
+const mymap = L.map("mapid", options).setView([-27.2151682, -49.642853], 15);
 
 //create and add title layer
 L.tileLayer(
@@ -23,14 +23,48 @@ L.tileLayer(
 //create icon
 const icon = L.icon({
     iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58,68],
-    iconAnchor: [2,68],
-    popupAnchor: [170,2]
+    iconSize: [58, 68],
+    iconAnchor: [2, 68],
+    popupAnchor: [170, 2]
 
 });
 
 
 
 
-L.marker([-27.2151682, -49.642853],{ icon})
-.addTo(mymap);
+L.marker([-27.2151682, -49.642853], {
+        icon
+    })
+    .addTo(mymap);
+
+
+/* image gallery */
+
+function selectImage(event) {
+    const button = event.currentTarget;
+
+    // remover todas as classes .active
+    const buttons = document.querySelectorAll(".images button")
+
+    buttons.forEach(removeActiveClass)
+
+
+    function removeActiveClass(button){
+        button.classList.remove(".active")
+    }
+
+    //selecionar a imagem clicada layer
+    const image = button.children[0]
+    const imageContainer = document.querySelector(".orphanage-details > img")
+
+
+
+    //atualizar o container de image
+    imageContainer.src = image.src
+
+
+    // adicionar a classe .active para este botao
+    button.classList.add('active')
+
+
+}
