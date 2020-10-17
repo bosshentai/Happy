a5219493beadadade6a04e1ba6cabc1fcc7064a6
bosshentai <baptistamhernani@gmail.com>
2020-10-17 16:56:53 -0100

    dia 4 ja terminod

---
+++ src/database/fakedata.js
@@ -0,0 +1,41 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-27.2151682",
+        lng: "-49.642853",
+        name: "Lar das meninas ",
+
+        description: "Presta assistencia a criança de 06 a 15 anos que se encontre em situaçao de risco e/ou vulnerabilidade social",
+
+        images: [
+
+            "https://images.unsplash.com/photo-1590009617786-6d054a2a3c7c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1594007184512-2a607d1df588?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciencia para dar",
+        opening_hours: "Horario de visitas das 18h ate 8h",
+        open_on_weekends: "1"
+
+
+    },
+
+    {
+        id: 2, 
+
+        lat: "-27.2151682",
+        lng: "-49.652853",
+        name: "Lar dos meninos ",
+        description: "Presta assistencia a criança de 06 a 15 anos que se encontre em situaçao de risco e/ou vulnerabilidade social",
+        images: [
+
+            "https://images.unsplash.com/photo-1590009617786-6d054a2a3c7c?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "https://images.unsplash.com/photo-1594007184512-2a607d1df588?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        ],
+
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciencia para dar",
+        opening_hours: "Horario de visitas das 18h ate 8h",
+        open_on_weekends: "0"
+
+    }
+
+]
--- src/server.js
+++ src/server.js
@@ -1,16 +1,25 @@
 // import dependencies
 const express = require('express');
 const path = require('path');
+const pages = require('./pages.js');
 // init o expresss
 const server = express()
 
 server
 // using the statics files
 .use(express.static('public'))
+
+//configure themeplate engine
+.set('views',path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
 //create route
-.get('/', (request,response) => {
-    return response.sendFile(path.join(__dirname,'views', 'index.html'))
-})
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+
+
 
 // connecto to server
 server.listen(5500)
